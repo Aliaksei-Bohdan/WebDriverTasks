e82ca83d92a46dd52682de6db6b57a6d9578e240
Aliaksei Bohdan <Aliaksei_Bohdan@epam.com>
2019-10-11 18:00:11 +0300

    added promises for mocha

--- using-mocha.js
+++ using-mocha.js
@@ -5,20 +5,20 @@ var webdriver = require('selenium-webdriver'),
 
     describe('library app scenarios', function() {
         this.timeout(50000);
-        beforeEach(function() {
-            driver = new webdriver.Builder().forBrowser('chrome').build();
+        beforeEach(async () => {
+            driver = await new webdriver.Builder().forBrowser('chrome').build();
         });
 
-        afterEach(function() {
-            driver.quit();
+        afterEach(async () => {
+            await driver.quit();
         });
 
-        it('submitting email shows an alert', function() {
-            driver.get('https://library-app.firebaseapp.com/');
-            driver.findElement(By.css('input')).sendKeys('us@mail.ru');
-            let submitBtn = driver.wait(until.elementLocated(By.css('.btn-lg')),5000);
-            submitBtn.click();
-            driver.wait(until.elementLocated(By.css('.alert-success')),15000).getText().then(function(txt){
+        it('submitting email shows an alert', async () => {
+            await driver.get('https://library-app.firebaseapp.com/');
+            await driver.findElement(By.css('input')).sendKeys('us@mail.ru');
+            let submitBtn = await driver.wait(until.elementLocated(By.css('.btn-lg')),5000);
+            await submitBtn.click();
+            await driver.wait(until.elementLocated(By.css('.alert-success')),15000).getText().then(function(txt){
                 console.log("Alert success test is: " + txt);
             });
         });
